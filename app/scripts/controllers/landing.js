bab0ae57c1e1b76361c4326dc3493251dbea1e3c
Billy Shen <billy.shen@hetic.net>
2016-01-30 15:01:27 +0100

    [ADD] Landing page

---
+++ app/scripts/controllers/landing.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flppnApp.controller:LandingCtrl
+ * @description
+ * # LandingCtrl
+ * Controller of the flppnApp
+ */
+angular.module('flppnApp')
+  .controller('LandingCtrl', function ($scope) {
+
+    console.log('LandingCtrl');
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flppnApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flppnApp
+ */
+angular.module('flppnApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
